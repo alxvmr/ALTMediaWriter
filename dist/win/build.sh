adbb92789b8bab3a9545476d7e002788012073bc
Dmitry Degtyarev <kvelyognaut@gmail.com>
2020-02-26 14:52:25 +0000

    change win build to be fully in msys
    remove 64bit build
    add installing required packages
    add building within script
    add new required dll's

--- dist/win/build.sh
+++ dist/win/build.sh
@@ -1,42 +1,80 @@
 #!/bin/bash
 
-# Before running this script
-# 
-# Get liblzma(compiled for mingw)
-#   copy to Qt/(version)/(mingw+version) AND Qt/Tools/(mingw+version)
-# 
-# Launch Qt commandline for mingw
-# (In root of the app)
-# qmake mediawriter.pro
-# mingw32-make
-# windeployqt --release app/release/mediawriter.exe
-# 
-# Copy all files inside dist/win/dlls into app/release
-# 
-# Launch MSYS2's mingw64 shell
-# Install git, dos2unix and nsis with
-#     pacman -S git dos2unix mingw-w64-x86_64-nsis
-# Run build.sh
+# Usage
+# Run this script from the mingw32 shell found in MSYS folder
+# ./build.sh - builds and deploys
+# ./build.sh install - install necessary packages
+# ./build.sh build - compiles the executable and copies necessary dll's
+# ./build.sh deploy - creates an installer
 
+# Check that we're running script from it's directory
 if [ ! -f build.sh ]
 then
 	echo "Error: run build.sh from it's directory"
 	exit 1
 fi
 
-BUILD_64BIT=false
-NSIS="C:/Program Files (x86)/NSIS/makensis.exe"
-if [ $BUILD_64BIT = true ]
+# Determine which step(or all) is executed by checking first arg
+opt_install=false
+opt_build=false
+opt_deploy=false
+
+if [ $# -gt 0 ]
 then
-    NSIS="makensis"
+    case "$1" in
+        install) opt_install=true
+            ;;
+        build) opt_build=true
+            ;;
+        deploy) opt_deploy=true
+            ;;
+    esac
+else
+	opt_build=true
+	opt_deploy=true
 fi
 
+# 
+# Install
+# 
+if [ $opt_install = true ]
+then
+	PACKAGES="base-devel git dos2unix mingw-w64-i686-toolchain mingw-w64-i686-xz mingw-w64-i686-qt5 mingw-w64-i686-nsis"
+
+	pacman -S --needed --noconfirm $PACKAGES
+fi
+
+# Setup paths
 SCRIPTDIR=$(pwd -P)
 ROOTPATH=$(realpath "$SCRIPTDIR/../../")
 BUILDPATH="$ROOTPATH/app/release"
 MEDIAWRITER="$ROOTPATH/app/release/mediawriter.exe"
 HELPER="$ROOTPATH/app/helper.exe"
 
+# 
+# Build
+# 
+
+if [ $opt_build = true ]
+then
+	cd "$ROOTPATH"
+	qmake -r "CONFIG+=release" mediawriter.pro
+	mingw32-make
+	windeployqt app/release/mediawriter.exe
+	cp dist/win/dlls/* app/release/
+fi
+
+# 
+# Deploy
+# 
+
+# Exit if not deploying
+if [ $opt_deploy = false ]
+then
+	exit
+fi
+
+# Check that executables were built
 if [ ! -f "$MEDIAWRITER" ] || [ ! -f "$HELPER" ]
 then
     echo "$MEDIAWRITER or $HELPER doesn't exist."
@@ -74,7 +112,7 @@ sed -i "s/#!define VERSIONBUILD/!define VERSIONBUILD $VERSION_BUILD/" "$SCRIPTDI
 sed -i "s/#!define INSTALLSIZE/!define INSTALLSIZE $INSTALLED_SIZE/" "$SCRIPTDIR/mediawriter.tmp.nsi"
 
 # Run .nsi script
-"$NSIS" "$SCRIPTDIR/mediawriter.tmp.nsi" >/dev/null
+makensis "$SCRIPTDIR/mediawriter.tmp.nsi" >/dev/null
 rm "$SCRIPTDIR/mediawriter.tmp.nsi"
 
 echo "Composing installer complete"
