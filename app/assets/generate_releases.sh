3b11e31a2c3e15534919f5b577c5944c33628708
Dmitry Degtyarev <kevl@altlinux.org>
2020-03-10 19:00:25 +0400

    add generate_releases.sh
    generate new releases.json using the script

---
+++ app/assets/generate_releases.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+parse_dir() {
+    # Parse a directory from a url into an array of items
+    url=$1
+    echo `curl -sSL -w -L '%{http_code} %{url_effective}' $url | grep href | sed 's/.*href="//' | sed 's/".*//' | grep '^[a-zA-Z].*'`
+}
+
+OUTPUT="releases.json"
+
+# Clear and recreate target file
+rm $OUTPUT
+touch $OUTPUT
+
+json_string="["
+add_comma=false
+
+GOOD_ARCHES="x86_64 aarch64 ppc64le i586"
+
+# Extract image urls from the mirrors directory
+for variant in "workstation" "server" "education" "simply"
+do
+    # echo "variant = $variant"
+    
+    variant_url="https://mirror.yandex.ru/altlinux/p9/images/$variant/"
+
+    # echo $variant_url
+    
+    arches=$(parse_dir $variant_url)
+    # NOTE: Remove "/" from the end of arches, need them to be clean for later processing
+    arches="${arches///}"
+    # echo "arches for $variant = $arches"
+
+    for arch in $arches
+    do
+        # Skip bad arches
+        if [[ $GOOD_ARCHES != *$arch* ]]
+        then
+           continue
+        fi
+
+        # echo "arch = $arch"
+
+        arch_url="$variant_url$arch"
+
+        images=$(parse_dir $arch_url)
+
+        for image in $images
+        do
+            # Skip non-image links
+            case $image in
+              *"SUM"*) continue ;;
+              *".txt"*) continue ;;
+              *".list"*) continue ;;
+            esac
+
+            # Convert image to fields for json
+            link="$arch_url/$image"
+            # echo "link = $link"
+            # echo "variant = $variant"
+            # echo "arch = $arch"
+
+            board="UNKNOWN"
+            case $arch in
+                "x86_64") board="pc";;
+                "i586") board="ibmpc";;
+                "ppc64le") board="powerpc";;
+                "aarch64")
+                    case $image in
+                        *"tegra"*) board="jetson-nano";;
+                        *"rpi4"*) board="rpi4";;
+                        *"tar.xz"*) board="rpi3";;
+                        *"iso"*) board="arm";;
+                        *) board="UNKNOWN AARCH64";;
+                    esac
+                    ;;
+                *) board="UNKNOWN"
+            esac
+
+            # Extract image type after "$arch." portion
+            imageType=$(echo $image | sed "s/.*$arch.//g")
+            # echo "imageType = $imageType"
+
+            if [ $add_comma == true ]
+            then
+                json_string="${json_string},"
+            else
+                add_comma=true
+            fi
+
+            # NOTE: version hardcoded, can't extract it from url directly because it's position varies and sometimes it's not there at all
+            json_string="${json_string}\n{\n\"link\":\"$link\",\n\"variant\":\"$variant\",\n\"version\":\"9.0\",\n\"arch\":\"$arch\",\n\"board\":\"$board\",\n\"imageType\":\"$imageType\",\n\"md5\":\"\"\n}"
+        done
+    done
+done
+
+json_string="${json_string}\n]"
+
+printf "$json_string" >> $OUTPUT
+
